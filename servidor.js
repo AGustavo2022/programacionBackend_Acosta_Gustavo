f464bfeb10d4694f874f3ad1eb2899f16b4b1db4
Acosta Gustavo <gustavo080487@gmail.com>
2022-11-15 10:21:05 -0300

    motoresDePlantillas

--- servidor.js
+++ servidor.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const { routerApi } = require('./routers/routerApi.js');
-const { randomUUID } = require('crypto');
+const { engine } = require('express-handlebars')
+
+const productos = [];
 
 const app = express();
 
 // middlewares
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
 // rutas
 app.use('/api/productos', routerApi);
+
+//
+app.get('/', (req, res) => {res.render('index');});
+app.get('/productos2', (req, res) => {
+    res.render('productos', { productos, hayProductos: productos.length > 0 });
+});
+
+
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+    console.log(productos)
+    res.redirect('/')
+});
+
+
+
 
 
 function conectar(puerto = 0) {
